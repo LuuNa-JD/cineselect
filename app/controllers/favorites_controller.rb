1055b2cadafc6f3c41377e2097eeeb7976527581
joachim miot <joachimmt55@gmail.com>
2023-11-25 15:25:09 -0500

    ADD favoris et delete

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,23 +1,26 @@
 class FavoritesController < ApplicationController
 
   def index
-    tmdb_service = ThemoviedbService.new(api_key)
-    @favorites_details = current_user.favorites.map do |favorite|
-      detail = if favorite.media_type == 'film'
-                tmdb_service.get_movie_details(favorite.tmdb_id)
-              elsif favorite.media_type == 'series'
-                tmdb_service.get_series_details(favorite.tmdb_id)
-              end
+    # tmdb_service = ThemoviedbService.new(api_key)
+    # @favorites_details = current_user.favorites.map do |favorite|
+    #   detail = if favorite.media_type == 'film'
+    #             tmdb_service.get_movie_details(favorite.tmdb_id)
+    #           elsif favorite.media_type == 'series'
+    #             tmdb_service.get_series_details(favorite.tmdb_id)
+    #           end
 
-      logger.debug "Favorite detail for tmdb_id #{favorite.tmdb_id}: #{detail.inspect}"
-      detail
-    end.compact
+    #   logger.debug "Favorite detail for tmdb_id #{favorite.tmdb_id}: #{detail.inspect}"
+    #   detail
+    # end.compact
+    @favorites = Favorite.where(user: current_user)
   end
+
 
 
   def create
     @favorite = Favorite.new(favorite_params)
     @favorite.user = current_user
+
 
     authorize @favorite
 
@@ -28,10 +31,17 @@ class FavoritesController < ApplicationController
     end
   end
 
+  def destroy
+    @favorite = Favorite.find(params[:id])
+    @favorite.destroy
+    authorize @favorite
+    redirect_to favorites_path, notice: 'votre film a été supprimé de vos favoris'
+  end
+
   private
 
   def favorite_params
-    params.permit(:tmdb_id, :media_type)
+    params.permit(:tmdb_id, :media_type, :title, :image_url)
   end
 
 
