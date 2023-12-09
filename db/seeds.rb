41a155edbb237d77dcab220909c95e86113b9f40
Martin Kevin Gapie <martinkevingapie@gmail.com>
2023-12-09 17:45:16 -0500

    platform design patch

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Favorite.destroy_all
+UserPlatform.destroy_all
+User.destroy_all
+User.create!(email: "cineselect@gmail.com", password: 123456, username: "Maurice")
