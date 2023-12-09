2e6d3dd39220cbe8f86f6098d122bcd851f980ca
Julien Denizot <luunz71@gmail.com>
2023-12-09 10:26:43 -0500

    patch js bug

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require jquery
-//= require jquery_ujs
 import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
 import "@fortawesome/fontawesome-free";
-// import "jquery3"
--- app/javascript/controllers/landing_controller.js
+++ app/javascript/controllers/landing_controller.js
@@ -1,16 +1,14 @@
-// import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus"
 
-// // Connects to data-controller="landing"
-// export default class extends Controller {
-//   connect() {
+export default class extends Controller {
+    connect() {
 
-    setTimeout(() => {
-      this.redirect();
-    }, 4000);
-  }
+      setTimeout(() => {
+        this.redirect();
+      }, 6000);
+    }
 
-//   redirect() {
-
-//     window.location.href = "/seances/new";
-//   }
-// }
+    redirect() {
+      window.location.href = "/seances/new";
+    };
+}
