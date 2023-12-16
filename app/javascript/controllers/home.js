c5d1951e99679ec3044a4bf247e2869bc2918d03
Martin Kevin Gapie <martinkevingapie@gmail.com>
2023-12-16 18:33:15 -0500

    add stimulus for the spinner to fix infinite load after logout

--- app/javascript/controllers/home.js
+++ app/javascript/controllers/home.js
@@ -4,16 +4,16 @@ $(document).ready(function() {
   setTimeout(function() {
     $("#slogan").fadeIn(1000).animate({ top: '50px', opacity: 1 }, { duration: 800, easing: 'easeInOutCubic' });
 
-    setTimeout(function() {
-      $(".loader").fadeIn(5000, function() {
+    // setTimeout(function() {
+    //   $(".loader").fadeIn(5000, function() {
 
-        $("#bouton").removeClass("d-none").addClass("bouton");
+    //     // $("#bouton").removeClass("d-none").addClass("bouton");
 
 
-        $("#bouton").css("opacity", 0).animate({ opacity: 1 }, 500);
+    //     // $("#bouton").css("opacity", 0).animate({ opacity: 1 }, 500);
 
-        $(".loader").fadeOut(0);
-      });
-    }, 1000);
+    //     // $(".loader").fadeOut(0);
+    //   });
+    // }, 1000);
   }, 2000);
 });
